706f992f2139d0c140077eb8536a3b99e1200f54
johnny <johnnysoto961@hotmail.com>
2020-07-12 23:30:59 -0500

    ViewData changes

--- BookStore/BookStore/Controllers/HomeController.cs
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -3,19 +3,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Dynamic;
+using BookStore.Models;
 
 namespace BookStore.Controllers
 {
     public class HomeController : Controller
     {
+        [ViewData]
+        public string PropertyName { get; set; }
         public ViewResult Index() 
         {
-            //return "Home Controller"; 
-            //var obj = new { Id = 22, Name = "Paul" };
+            //ViewBag.Title = "Johnny";
+
+            //dynamic data = new ExpandoObject(); // #32
+            //data.Id = 1;
+            //data.Name = "Jermyy";
+
+            //ViewBag.Type = new BookModel() { Id = 5, Author = "Kevin" };
+
+            //ViewBag.Data = data;
+            ViewData["property1"] = "Jhony ViewData";
+
+            ViewData["book"] = new BookModel() { Author = "Johnny", Id = 19 };
+
             return View();
         }
         public ViewResult AboutUs()
         {
+            PropertyName = "Johnny S";
+            ViewData["PropertyName"] = PropertyName;
             return View();
         }
 
